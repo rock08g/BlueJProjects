52e5a1409c76ec18ffeadd09e5ba7ad150c64ebd
rock08g <105604953+rock08g@users.noreply.github.com>
2023-12-25 23:42:17 -0500

    Update FindingWebLinks.java

--- FindingWebLinks.java
+++ FindingWebLinks.java
@@ -1,1 +1,20 @@
 import edu.duke.*;
+
+public class FindingWebLinks {
+    public void findingWebLinks () {
+        URLResource ur = new URLResource("https://www.dukelearntoprogram.com//course2/data/manylinks.html");
+        String yt = "youtube.com";
+        String quote = "\"";
+        for (String word : ur.words()) {
+            String tempURL = word.toLowerCase();
+            int startIndex = tempURL.indexOf(yt);
+            if (startIndex != -1) {
+                int quoteStartIndex = tempURL.indexOf(quote);
+                int quoteEndIndex = tempURL.indexOf(quote, startIndex + 1);
+                String ytURL = word.substring(quoteStartIndex + 1, quoteEndIndex);
+                System.out.println(ytURL);
+            }
+            
+        }
+    }
+}
