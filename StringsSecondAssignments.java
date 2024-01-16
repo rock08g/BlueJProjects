51a1c1e4c50dc9a965748c193d52d9a4cacacced
rock08g <105604953+rock08g@users.noreply.github.com>
2024-01-16 16:27:57 -0500

    added part 2 and part 3

--- StringsSecondAssignments.java
+++ StringsSecondAssignments.java
@@ -4,7 +4,6 @@ public class Part1 {
     public int findStopCodon(String dna, int startIndex, String stopCodon) {
         int index = dna.indexOf(stopCodon, startIndex + 3);
         while (index != -1) {
-
             if ((index - startIndex) % 3 == 0) {
                 return index;
             }
@@ -14,9 +13,9 @@ public class Part1 {
         }
         return dna.length();
     }
-}
+
     
-public String findGene (String dna) {
+    public String findGene (String dna) {
         int startCodon = dna.indexOf("ATG", 0);
         if (startCodon == -1) {
             return "";
@@ -26,9 +25,20 @@ public String findGene (String dna) {
         int tgaCodon = findStopCodon(dna, startCodon, "TGA");
         int minIndex = Math.min(taaCodon, Math.min(tagCodon, tgaCodon));
         if (minIndex == -1) {
-            return "";
+           return "";
         }
         return dna.substring(startCodon, minIndex + 3);
+    }
+
+    public void printAllGenes (String dna) {
+        while (true) {
+            String gene = findGene(dna);
+            if (gene.length() == 0) {
+                break;
+            }
+            System.out.println(gene);
+            dna = dna.substring(gene.length());
+        }
     }
 
     public void testFindGene() {
@@ -65,38 +75,88 @@ public String findGene (String dna) {
 
     public void testFindStopCodon () {
         String dna1 = "ATGAAATAGTAA";
-        String result1 = findStopCodon(dna1, 0, "TAA");
+        int result1 = findStopCodon(dna1, 0, "TAA");
         System.out.println("DNA: " + dna1);
         System.out.println("Stop Codon: TAA");
         System.out.println("Result: " + result1);
         System.out.println();
 
         String dna2 = "ATGAAATAGTAG";
-        String result2 = findStopCodon(dna2, 0, "TAG");
+        int result2 = findStopCodon(dna2, 0, "TAG");
         System.out.println("DNA: " + dna2);
         System.out.println("Stop Codon: TAG");
         System.out.println();
-        String result3 = findStopCodon(dna3, 0, "TGA");
-
-        String stopCodon3 = "TGA";
-        int result3 = findStopCodon(dna3, 0, stopCodon3);
+          
+        String dna3 = "TGA";
+        int result3 = findStopCodon(dna3, 0, "TGA");
         System.out.println("DNA: " + dna3);
         System.out.println("Stop Codon: TGA");
         System.out.println("Result: " + result3);
         System.out.println();
 
         String dna4 = "ATGAAATAGT";
-        String result4 = findStopCodon(dna4, 0, "TAA");
+        int result4 = findStopCodon(dna4, 0, "TAA");
         System.out.println("DNA: " + dna4);
         System.out.println("Stop Codon: TAA");
         System.out.println();
-        String result5 = findStopCodon(dna5, 0, "TAA");
-
-        String stopCodon5 = "TAA";
-        int result5 = findStopCodon(dna5, 0, stopCodon5);
+        
+        String dna5 = "TAA";
+        int result5 = findStopCodon(dna5, 0, "TAA");
         System.out.println("DNA: " + dna5);
         System.out.println("Stop Codon: TAA");
         System.out.println("Result: " + result5);
         System.out.println();
+    }
+}
+
+public class Part2 {
+    public int howMany(String stringa, String stringb) {
+        int count = 0;
+        int index = stringb.indexOf(stringa);
+        while (index!= -1) {
+            count++;
+            index = stringb.indexOf(stringa, index + stringa.length());
+        }
+        return count;
+    }
+
+    public void testHowMany() {
+        String stringa1 = "GAA";
+        String stringb1 = "ATGAACGAATTGAATC";
+        int result1 = howMany(stringa1, stringb1);
+        System.out.println("Stringa: " + stringa1);
+        System.out.println("Stringb: " + stringb1);
+        System.out.println("Result: " + result1);
+        System.out.println();
+        String stringa2 = "AA";
+        String stringb2 = "ATAAAA";
+        int result2 = howMany(stringa2, stringb2);
+        System.out.println("Stringa: " + stringa2);
+        System.out.println("Stringb: " + stringb2);
+        System.out.println("Result: " + result2);
+        System.out.println();
+    }
+}
+
+public class Part3 {
+    public int countGenes(String dna) {
+        int startCodon = dna.indexOf("ATG", 0);
+        if (startCodon == -1) {
+            return "";
+        }
+        int count = 0;
+        int taaCodon = findStopCodon(dna, startCodon, "TAA");
+        int tagCodon = findStopCodon(dna, startCodon, "TAG");
+        int tgaCodon = findStopCodon(dna, startCodon, "TGA");
+        int minIndex = Math.min(taaCodon, Math.min(tagCodon, tgaCodon));
+        if (minIndex == -1) {
+           return "";
+        }
+        return dna.substring(startCodon, minIndex + 3);
+    }
+
+    public void testCountGenes() {
+        int result = countGenes("ATGTAAGATGCCCTAGT");
+        System.out.println("There were " + result + " genes in the DNA.");
     }
 }
